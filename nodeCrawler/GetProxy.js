7428e0f104c0de77564454ccedaf0fd9b8e2cc45
Tai, WU <tai.wu@sap.com>
2017-10-10 23:17:31 +0800

    proxy change

--- nodeCrawler/GetProxy.js
+++ nodeCrawler/GetProxy.js
@@ -4,7 +4,7 @@ var cheerio = require('cheerio');
 var Q = require("q");
 var verifyTimeout = 3000;
 function GetProxy(){
-  this.apiurl = "http://api.goubanjia.com/api/get.shtml?order=ab8a0dc28f69eafe14b26a8f91b01637&num=30&carrier=0&protocol=1&an1=1&an2=2&an3=3&sp1=1&sp2=2&sort=2&system=1&distinct=0&rettype=1&seprator=%0D%0A";
+  this.apiurl = "http://api.goubanjia.com/api/get.shtml?order=ab8a0dc28f69eafe14b26a8f91b01637&num=300&carrier=0&protocol=1&an1=1&an2=2&an3=3&sp1=1&sp2=2&sort=2&system=1&distinct=0&rettype=1&seprator=%0D%0A";
   this.currentSetOfProxyList = [];
   this.validProxyList = [];
   this.notValidProxyList = [];
